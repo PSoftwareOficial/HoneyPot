239297074d048a570a7de10597a8ad9a6fefad8a
PSoftwareOficial <psoftwareoficial@gmail.com>
2025-08-12 22:12:48 +0000

    Fixes

--- app/src/main/cpp/Engine.cpp
+++ app/src/main/cpp/Engine.cpp
@@ -36,9 +36,9 @@ void Engine::Loop() {
 		}
 		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 	}
-	LOG("Ending Engine")
+	LOG("Ending Engine");
 	Engine::engine->Destroy();
-	LOG("Engine Destoryed")
+	LOG("Engine Destoryed");
 }
 
 
