86a4d136efe4a4b07df4fce663fdd68c76364fde
PSoftwareOficial <psoftwareoficial@gmail.com>
2025-08-12 21:54:16 +0000

    Fixes

--- app/src/main/cpp/custom/UI/Button.h
+++ app/src/main/cpp/custom/UI/Button.h
@@ -6,6 +6,9 @@ class Button : public BaseUI {
     public:
         virtual void Init(V2D PosParam,V2D SizeParam,V3Du8 ColorParam);
         virtual void Draw() override;
-        V3Du8 color;
+        virtual void GetGLData(float (&data)[20]);
+
         virtual ~Button() override {}
+
+        V3Du8 color;
 };
