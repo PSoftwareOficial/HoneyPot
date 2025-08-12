5bcec319ab8da4b4400d5c57eab8987d6d398837
PSoftwareOficial <psoftwareoficial@gmail.com>
2025-08-12 21:51:52 +0000

    Fixes

--- app/src/main/cpp/custom/UI/Button.h
+++ app/src/main/cpp/custom/UI/Button.h
@@ -2,7 +2,7 @@
 
 #include "BaseUI.h"
 
-class Button : BaseUI {
+class Button : public BaseUI {
     public:
         virtual void Init(V2D PosParam,V2D SizeParam,V3Du8 ColorParam);
         virtual void Draw() override;
