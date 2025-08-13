16f7d51e0fcb6f1c1f911c4e2b4aec4764c685f4
PSoftwareOficial <psoftwareoficial@gmail.com>
2025-08-13 08:14:57 +0000

    Fixes

--- app/src/main/cpp/utilities/Buffer/Buffer.h
+++ app/src/main/cpp/utilities/Buffer/Buffer.h
@@ -11,16 +11,16 @@ public:
         return nElem.load();
     }
     void AddElem(const T& elem){
-        std::lock_guard(mutex);
+        std::lock_guard<std::mutex> lock(mutex);
         objs[(idx.load() + nElem.load()) % N].store(elem);
-        nElem.fetch_add();
+        nElem.fetch_add(1);
     }
     void PopElem(T& elem){
-        std::lock_guard(mutex);
+        std::lock_guard<std::mutex> lock(mutex);
         if(!GetElemNum.load()) return;
         elem = objs[idx.load()].load();
         idx.store((idx.load() + 1) % N);
-        nElem.fetch_sub();
+        nElem.fetch_sub(1);
     }
 private:
     std::atomic<T> objs[N];
