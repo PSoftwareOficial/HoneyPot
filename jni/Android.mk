c3cc9c892e11ff3a2c864e0fce3def5fed0968e7
PSoftwareOficial <psoftwareoficial@gmail.com>
2025-08-18 16:17:02 +0000

    Starting Text Rendering Implementation

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,6 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 
-# First, build native_app_glue as a static library
+# -------------------------------
+# Build native_app_glue as a static library
+# -------------------------------
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := native_app_glue
@@ -9,24 +11,54 @@ LOCAL_EXPORT_C_INCLUDES := $(NDK_ROOT)/sources/android/native_app_glue
 
 include $(BUILD_STATIC_LIBRARY)
 
-# Now build the main shared library
+# -------------------------------
+# Build FreeType as a static library
+# -------------------------------
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := freetype
+LOCAL_SRC_FILES := \
+    ../app/src/main/cpp/rendering/freetype/src/autofit/autofit.c \
+    ../app/src/main/cpp/rendering/freetype/src/base/ftbase.c \
+    ../app/src/main/cpp/rendering/freetype/src/base/ftbitmap.c \
+    ../app/src/main/cpp/rendering/freetype/src/base/ftglyph.c \
+    ../app/src/main/cpp/rendering/freetype/src/base/ftinit.c \
+    ../app/src/main/cpp/rendering/freetype/src/base/ftsystem.c \
+    ../app/src/main/cpp/rendering/freetype/src/smooth/ftsmooth.c \
+    ../app/src/main/cpp/rendering/freetype/src/truetype/truetype.c \
+    ../app/src/main/cpp/rendering/freetype/src/raster/raster.c \
+    ../app/src/main/cpp/rendering/freetype/src/cache/ftcache.c
+
+LOCAL_C_INCLUDES := \
+    ../app/src/main/cpp/rendering/freetype/include \
+    ../app/src/main/cpp/rendering/freetype/src
+
+LOCAL_CFLAGS := -Wall -Wno-unused-function -Wno-unused-variable
+
+include $(BUILD_STATIC_LIBRARY)
+
+# -------------------------------
+# Build main shared library
+# -------------------------------
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := baseNativeLib
 LOCAL_SRC_FILES := \
-../app/src/main/cpp/Engine/Engine.cpp \
-../app/src/main/cpp/custom/UI/UIManager.cpp
+    ../app/src/main/cpp/Engine/Engine.cpp \
+    ../app/src/main/cpp/custom/UI/UIManager.cpp\
+    ../app/src/main/cpp/rendering/text/text.cpp
 
-#Just add the different cpp files.
 LOCAL_CFLAGS := -Wall -Wno-unused-function -Wno-unused-variable
 LOCAL_CPPFLAGS := -std=gnu++17
 LOCAL_LDLIBS := -llog -landroid -lEGL -lGLESv3
-LOCAL_STATIC_LIBRARIES := native_app_glue
+LOCAL_STATIC_LIBRARIES := native_app_glue freetype
 
 # Workaround to export ANativeActivity_onCreate
 LOCAL_LDFLAGS := -u ANativeActivity_onCreate
 
 LOCAL_C_INCLUDES := \
-    $(NDK_ROOT)/sources/android/native_app_glue
+    $(NDK_ROOT)/sources/android/native_app_glue \
+    ../app/src/main/cpp/rendering/freetype/include \
+    ../app/src/main/cpp/rendering/freetype/src
 
 include $(BUILD_SHARED_LIBRARY)
