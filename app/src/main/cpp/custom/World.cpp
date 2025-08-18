02285cfc282b4ad7d4008ff7c719a667261842e2
PSoftwareOficial <psoftwareoficial@gmail.com>
2025-08-18 18:55:41 +0000

    Fixes

--- app/src/main/cpp/custom/World.cpp
+++ app/src/main/cpp/custom/World.cpp
@@ -15,6 +15,7 @@ int World::Update(uint64_t EuS, uint64_t TuS) {
 
 int World::Draw() {
         uiManager.Draw();
+        glEnable(GL_BLEND);
         textRenderer.DrawText(V2D{-0.5f,0.5f},V2D{0.1f, 0.1f},"Hello GL");
         return 0;
     }
