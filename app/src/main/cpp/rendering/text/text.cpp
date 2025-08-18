405cb5e2f54045b4239d9a4f0231b959c2eddb49
PSoftwareOficial <psoftwareoficial@gmail.com>
2025-08-18 20:00:04 +0000

    Testing

--- app/src/main/cpp/rendering/text/text.cpp
+++ app/src/main/cpp/rendering/text/text.cpp
@@ -36,24 +36,21 @@ void TextRenderer::InitGL(){
     glUseProgram(SHADER.program);
     GLCheck("Setting Program");
 
+    // Generate buffers
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
 
+    // Upload vertex data to GPU
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    // Position attribute
+    // Set vertex attribute pointers
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
     glEnableVertexAttribArray(0);
 
-    // Unbind VAO
-    glBindVertexArray(0);
+    glBindVertexArray(0); // Unbind VAO
 
 }
 
@@ -66,7 +63,7 @@ void TextRenderer::DrawText(V2D Pos, V2D TextSize, const std::string& text){
         // Draw the square
         glBindVertexArray(VAO);
         GLCheck("Setting VAO");
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 4);  // Starting from vertex 0, drawing 4 vertices
         GLCheck("Drawing Elements");
         glBindVertexArray(0);
 }
