997203af56d951bec54a47fbb822f3a21ec378b9
PSoftwareOficial <psoftwareoficial@gmail.com>
2025-08-18 18:40:47 +0000

    Tsting

--- app/src/main/cpp/custom/World.cpp
+++ app/src/main/cpp/custom/World.cpp
@@ -3,13 +3,13 @@
 int World::InitGL() {
     
     uiManager.InitGL();
-    textRenderer.InitGL();
+    //textRenderer.InitGL();
     return 0;
 }
 
 
 int World::Update(uint64_t EuS, uint64_t TuS) {
     uiManager.Update();
-    textRenderer.DrawText(V2D{-0.5f,0.5f},V2D{0.1f, 0.1f},"Hello GL");
+    //textRenderer.DrawText(V2D{-0.5f,0.5f},V2D{0.1f, 0.1f},"Hello GL");
     return 0;
 }
