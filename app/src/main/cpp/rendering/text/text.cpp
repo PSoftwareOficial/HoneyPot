05abb4d5b30483aa75f6eb2e3f86a6dda6fb83ae
PSoftwareOficial <psoftwareoficial@gmail.com>
2025-08-19 11:15:09 +0000

    Tseting

--- app/src/main/cpp/rendering/text/text.cpp
+++ app/src/main/cpp/rendering/text/text.cpp
@@ -97,6 +97,14 @@ void TextRenderer::InitGL(){
 
     glGenTextures(1, &TEX);
     glBindTexture(GL_TEXTURE_2D, TEX);
+    GLCheck("BindTexture GlyphAtlas");
+    // Set texture parameters — very important!
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+
     texture2D tex;
     if(GetTexture("images/glyphAtlas.png",tex)){
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, tex.width, tex.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, tex.tex.get());
@@ -104,11 +112,7 @@ void TextRenderer::InitGL(){
         LOGI("TEXT RENDERER LOADED TEXTURE ATLAS: " + std::to_string(tex.width) + " " + std::to_string(tex.height));
     }
 
-    // Set texture parameters — very important!
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    
 
 }
 
