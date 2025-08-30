3e88bec00380ca18696ac9a2aa5dc5046e323b07
JannesUnal <jplett@unal.edu.co>
2025-08-30 17:46:09 +0200

    Testing

--- app/src/main/cpp/custom/UI/Keyboard.cpp
+++ app/src/main/cpp/custom/UI/Keyboard.cpp
@@ -4,7 +4,7 @@
 #include "../../rendering/text/text.h"
 #include "../../utilities/log/API.h"
 
-static constexpr bool debugKeyboard = false;
+static constexpr bool debugKeyboard = true;
 #define dbg(msg) if constexpr(debugKeyboard) LOGI_FL(msg)
 
 int KeyBoard::Init() {
